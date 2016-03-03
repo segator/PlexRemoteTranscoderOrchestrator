8df4e03d535f313c7007645237694b679428a38e
Isaac Aymerich <isaac.aymerich@gmail.com>
2016-03-03 21:58:54 +0100

    dynamic auth smb transcoder server

--- scripts/entrypoint-digitalocean.sh
+++ scripts/entrypoint-digitalocean.sh
@@ -5,9 +5,11 @@ export MACHINE_ID=$(curl -s http://169.254.169.254/metadata/v1/id)
 export MAIN_HOST='##mainhost##'
 export SMB_HOST='##smbhost##'
 export SMB_PORT='##smbport##'
+export SMB_USER='##smbuser##'
+export SMB_PASS='##smbpass##'
 export WEB_PORT='##webport##'
 
 #mount transcode ouput
-mount -t cifs -o user=WTa92r,pass=r4tuJH_JRQ,port=$SMB_PORT //$SMB_HOST/transcode /transcode
+mount -t cifs -o user=$SMB_USER,pass=$SMB_PASS,port=$SMB_PORT //$SMB_HOST/transcode /transcode
 #run machineRefresh Script
 screen -S machineNotifier -A -m -d python /usr/bin/machineRefresh.py $MAIN_HOST:$WEB_PORT
