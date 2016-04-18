73f9778367a6408f28b9d74d741bf40a682a4ed0
segator <isaac.aymerich@gmail.com>
2016-04-18 21:37:50 +0200

    reduced timeout time

--- src/main/java/org/segator/plex/cloud/transcoding/constants/TranscoderConstants.java
+++ src/main/java/org/segator/plex/cloud/transcoding/constants/TranscoderConstants.java
@@ -12,8 +12,7 @@ package org.segator.plex.cloud.transcoding.constants;
 public class TranscoderConstants {
 
     public static final String DROPLET_BASE_NAME = "Transcoder-";
-    public static long UNUSED_TIMEOUT_MACHINE = 30*60*1000;
-    //public static long UNUSED_TIMEOUT_MACHINE = 153425345 * 60 * 1000;
+    public static long UNUSED_TIMEOUT_MACHINE = 10*60*1000;
     public static String DROPLET_IMAGE_PLEX_REMOTE_TRANSCODING = "PlexRemoteTranscoder";
     public static String DROPLET_BASE_BUILD_SLUG = "debian-8-x64";
     public static String DROPLET_PROVISION_FILE_NAME = "/etc/transcoder_orchestrator/provide-digitalocean.sh";
